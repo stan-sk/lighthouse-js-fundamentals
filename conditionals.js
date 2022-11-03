2f077b31a345d56f2c2ecfed67df0872708cf4de
Stanley Lai <stanley.lai@telus.com>
2022-11-02 17:16:23 -0700

    Add more conditional examples

--- conditionals.js
+++ conditionals.js
@@ -22,7 +22,7 @@ if (cold) {
 } */
 
 
-const temperature = 12;
+/* const temperature = 12;
 
 if (temperature < 0) {
   console.log("Make sure you pick out a scarf!");
@@ -32,4 +32,24 @@ if (temperature < 0) {
   console.log("Short sleeves are fine.");
 }
 
-console.log("Now you're ready to go outside!");
+console.log("Now you're ready to go outside!"); */
+
+
+// Logical operators
+/* const isCitizen = true;
+const age = 26; */
+
+/* if (isCitizen && age > 18) {
+  console.log("You are eligible to vote.");
+} */
+
+/* let temperature = 42;
+if (temperature < -40 || temperature > 40) {
+  console.log("Maybe going outside isn't such a great idea…");
+} */
+
+let raining = false;
+
+if (!raining) {
+  console.log("Leave your umbrella at home!");
+}
