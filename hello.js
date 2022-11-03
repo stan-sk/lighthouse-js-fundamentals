e813f1ad99036f0e3994ed30038fc978d31bfb4e
Stanley Lai <stanley.lai@telus.com>
2022-11-03 15:56:12 -0700

    add hello and even js

---
+++ hello.js
@@ -0,0 +1,15 @@
+/* const sayHello  = function () {
+  console.log("Hello, world");
+}
+
+sayHello();
+
+const sayHelloToConsole  = function (name) {
+  console.log("Hello, " + name);
+}
+sayHelloToConsole('John'); */
+
+const returnSayHello  = function (name) {
+  return "Hello, " + name;
+}
+const greeting = returnSayHello('John');
